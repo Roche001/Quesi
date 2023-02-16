ffd5af37a84bc9217f290723a0b54ef8a3c01109
Roche Harold <rocheharold13@gmail.com>
2023-02-16 21:55:16 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,30 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      {" "}
+      <h4>
+        Subtitle 1: The Origins of Beer Beer is one of the oldest and most
+        widely consumed alcoholic beverages in the world. It is believed to have
+        originated in ancient Mesopotamia over 5,000 years ago, where it was
+        brewed using barley. The popularity of beer spread across Europe during
+        the Middle Ages, and it eventually became a staple of the Western world.
+        Subtitle 2: The Brewing Process The brewing process of beer involves
+        several steps, including malting, mashing, boiling, and fermentation.
+        During the malting process, grains are soaked in water and allowed to
+        germinate, which activates enzymes that convert starches into sugars.
+        The mash is then boiled with hops to give the beer its distinctive
+        bitter taste and aroma. Finally, the mixture is fermented with yeast to
+        produce alcohol. Subtitle 3: Types of Beer Beer comes in many different
+        types and flavors, from light and crisp lagers to dark and rich stouts.
+        Some of the most popular styles include pilsners, pale ales, IPAs,
+        porters, and wheat beers. Each type of beer has its unique
+        characteristics, such as color, bitterness, and alcohol content.
+        Subtitle 4: Health Benefits of Beer
+      </h4>
+    </div>
+  );
 };
 
 export default Home;
