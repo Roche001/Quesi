183b2e339deb7e543326c7679afea261f7fa46da
Roche Harold <rocheharold13@gmail.com>
2023-02-16 18:16:57 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./Top.css";
+import { GiGrapes } from "react-icons/gi";
 
 const Top = () => {
-  return <div>Top</div>;
+  return (
+    <div className="cork">
+      <div className="cork-photo">
+        <h4>
+          <GiGrapes />
+        </h4>
+      </div>
+      <div className="cork-text">
+        <h4>
+          Roche.<span>Liqour</span>
+        </h4>
+      </div>
+    </div>
+  );
 };
 
 export default Top;
