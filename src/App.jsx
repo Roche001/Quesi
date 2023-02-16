691d5ef91775015b8875b931d77932ee776104b6
Roche Harold <rocheharold13@gmail.com>
2023-02-16 19:59:11 +0300

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Top from "./components/top/Top";
+import Home from "./components/home/Home";
+import Beer from "./components/beer/Beer";
 
 const App = () => {
-  return <Top />;
+  return (
+    <Router>
+      <Top />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Beer />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
