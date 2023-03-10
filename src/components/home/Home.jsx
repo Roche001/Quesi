0f55ef9f1de96b1c4e201844da2f27dd3082d298
Roche Harold <rocheharold13@gmail.com>
2023-03-10 11:32:07 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,7 +4,13 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="banner"></div>
+      <div className="banner">
+        <h2>Order alcohol for delivery or pickup </h2>
+        <p>
+          Get your favorite beer, wine & spirits delivered to your door step
+        </p>
+        <input type="text" />
+      </div>
     </div>
   );
 };
