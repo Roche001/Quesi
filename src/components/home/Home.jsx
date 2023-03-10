87a6be6de60f6b7b5d6f7ec3e1613eb045318d69
Roche Harold <rocheharold13@gmail.com>
2023-03-10 22:47:35 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./Home.css";
 import { BsSearch } from "react-icons/bs";
+import { MdLocationPin } from "react-icons/md";
 import { beer } from "../../drinks";
+import { crates } from "../../drinks";
+import { homewine } from "../../drinks";
+import { homespirit } from "../../drinks";
+import { homeextras } from "../../drinks";
 import { Link } from "react-router-dom";
 
 const Home = () => {
@@ -23,14 +28,110 @@ const Home = () => {
       <div className="home-drinks">
         {beer.map((beer, index) => {
           return (
-            <div key={index} className="menu-one">
-              <Link to={beer.titleLink} id="menu-two">
+            <div key={index}>
+              <Link
+                to={beer.titleLink}
+                id="menu-two"
+                className="drinks-one rotate"
+              >
                 <img src={beer.titleImage} alt="liqour" loading="lazy" />
                 <h5>{beer.title}</h5>
               </Link>
             </div>
           );
         })}
+      </div>
+      <div className="home-category">
+        <h3>Shop by Category</h3>
+        <p>Start shopping by browshing your favorite category</p>
+        <div className="home-div">
+          <div className="home-beer">
+            <h2>Beer</h2>{" "}
+            <div className="home-beer-div">
+              {crates.map((crates, index) => {
+                return (
+                  <div key={index}>
+                    <Link to={crates.titleLink} id="menu-two">
+                      <h5>
+                        {crates.title} {crates.titleicon}
+                      </h5>
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="home-wine">
+            {" "}
+            <h2>Wine</h2>
+            <div className="home-wine-div">
+              {homewine.map((homewine, index) => {
+                return (
+                  <div key={index}>
+                    <Link to={homewine.titleLink} id="menu-two">
+                      <h5>
+                        {homewine.title} {homewine.titleicon}
+                      </h5>
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="homeextras">
+            {" "}
+            <h2>Extras</h2>
+            <div className="homeextras-div">
+              {homeextras.map((homeextras, index) => {
+                return (
+                  <div key={index}>
+                    <Link to={homeextras.titleLink} id="menu-two">
+                      <h5>
+                        {homeextras.title}
+                        {homeextras.titleicon}
+                      </h5>
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="homespirit">
+            {" "}
+            <h2>Spirits</h2>
+            <div className="homespirit-div">
+              {homespirit.map((homespirit, index) => {
+                return (
+                  <div key={index}>
+                    <Link to={homespirit.titleLink} id="menu-two">
+                      <h5>
+                        {homespirit.title} {homespirit.titleicon}
+                      </h5>
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="home-find">
+        <div className="home-find-one">
+          <h4>
+            Liqour stores in your neighbourhood{" "}
+            <span>
+              <MdLocationPin />
+            </span>{" "}
+          </h4>
+          <p>
+            Discover local liquor stores and order your favorite beer, wine,
+            liquor, and more online.{" "}
+          </p>
+          <button>Find Liqour Store</button>
+        </div>
+        <div className="home-find-two">
+          <img src="./assets/index.jpg" alt="liqour-store" />
+        </div>
       </div>
     </div>
   );
--- src/drinks.js
+++ src/drinks.js
@@ -1,3 +1,4 @@
+import { TiLocationArrow } from "react-icons/ti";
 export const beer = [
   {
     title: "Beer",
@@ -13,4 +14,50 @@ export const beer = [
   { title: "Cognac", titleImage: "./assets/beer8.jpg", titleLink: "/Cognac" },
   { title: "Brandy", titleImage: "./assets/beer9.jpg", titleLink: "/Brandy" },
   { title: "Extras", titleImage: "./assets/beer10.jpg", titleLink: "/Extras" },
+];
+export const crates = [
+  { title: "Domestic Beer", titleLink: "/DomesticBeer" },
+  { title: "Lager", titleLink: "/Lager" },
+  { title: "Imported Beer", titleLink: "/ImportedBeer" },
+  { title: "Ale", titleLink: "/Ale" },
+  { title: "Craft Beer", titleLink: "/CraftBeer" },
+  { title: "Non Alcoholic Beer", titleLink: "/Nonalcoholic" },
+  { title: "All Beer", titleLink: "/Beer", titleicon: <TiLocationArrow /> },
+];
+export const homewine = [
+  { title: "Red Wine", titleLink: "/Redwine" },
+  {
+    title: "White Wine",
+    titleLink: "/Whitewine",
+  },
+  {
+    title: "Sparkline Wine",
+    titleLink: "/Sparklinewine",
+  },
+  { title: "Champagne", titleLink: "/Champagne" },
+  { title: "Rose & Blush Wine", titleLink: "/Roseblush" },
+  { title: "Dessert & Fortified Wine", titleLink: "/Fortified" },
+  { title: "All Wine", titleLink: "/Wine", titleicon: <TiLocationArrow /> },
+];
+export const homeextras = [
+  { title: "Syrup", titleLink: "/Syrup" },
+  { title: "Mixer", titleLink: "/Mixer" },
+  { title: "Soda", titleLink: "/Soda" },
+  { title: "Juice", titleLink: "/Juice" },
+  { title: "Energy Drink", titleLink: "/Energy" },
+  { title: "Water", titleLink: "/Water" },
+  { title: "All Extras", titleLink: "/Extras", titleicon: <TiLocationArrow /> },
+];
+export const homespirit = [
+  { title: "Vodka", titleLink: "/Vodka" },
+  { title: "Tequila", titleLink: "/Tequila" },
+  { title: "Whiskey", titleLink: "/Whiskey" },
+  { title: "Gin", titleLink: "/Gin" },
+  { title: "Brandy", titleLink: "/Brandy" },
+  { title: "Cognac", titleLink: "/Cognac" },
+  {
+    title: "All Spirits",
+    titleLink: "/Spirits",
+    titleicon: <TiLocationArrow />,
+  },
 ];
