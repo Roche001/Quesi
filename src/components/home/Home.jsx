9661c54fd935393dbc5875b26d6c41a4c533f67d
Roche Harold <rocheharold13@gmail.com>
2023-03-10 14:23:22 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Home.css";
 import { BsSearch } from "react-icons/bs";
+import { beer } from "../../drinks";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -8,7 +10,7 @@ const Home = () => {
       <div className="banner">
         <h2>Order alcohol for delivery or pickup </h2>
         <p>
-          Get your favorite beer, wine & spirits delivered to your door step
+          Get your favorite beer, wine & spirits delivered to your door step.
         </p>
         <div className="fire">
           <input type="text" placeholder=" Enter Your Street or Home Address" />{" "}
@@ -17,6 +19,18 @@ const Home = () => {
             <BsSearch />
           </h3>
         </div>
+      </div>
+      <div className="home-drinks">
+        {beer.map((beer, index) => {
+          return (
+            <div key={index} className="menu-one">
+              <Link to={beer.titleLink} id="menu-two">
+                <img src={beer.titleImage} alt="liqour" loading="lazy" />
+                <h5>{beer.title}</h5>
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/drinks.js
@@ -0,0 +1,16 @@
+export const beer = [
+  {
+    title: "Beer",
+    titleImage: "./assets/beer1.jpg",
+    titleLink: "/Beer",
+  },
+  { title: "Wine", titleImage: "./assets/beer2.jpg", titleLink: "/Wine" },
+  { title: "Whiskey", titleImage: "./assets/beer3.jpg", titleLink: "/Whiskey" },
+  { title: "Tequila", titleImage: "./assets/beer4.jpg", titleLink: "/Tequila" },
+  { title: "Vodka", titleImage: "./assets/beer5.jpg", titleLink: "/Vodka" },
+  { title: "Rum", titleImage: "./assets/beer6.jpg", titleLink: "/Rum" },
+  { title: "Gin", titleImage: "./assets/beer7.jpg", titleLink: "/Gin" },
+  { title: "Cognac", titleImage: "./assets/beer8.jpg", titleLink: "/Cognac" },
+  { title: "Brandy", titleImage: "./assets/beer9.jpg", titleLink: "/Brandy" },
+  { title: "Extras", titleImage: "./assets/beer10.jpg", titleLink: "/Extras" },
+];
