6cfd9567c861c6adbb6edaf8e03fe7a776f6b0e2
Roche Harold <rocheharold13@gmail.com>
2023-04-17 15:00:03 +0300

    update changes

--- src/components/stores/Stores.jsx
+++ src/components/stores/Stores.jsx
@@ -41,21 +41,11 @@ const Stores = () => {
   return (
     <div className="store-container">
       <div className="stores-banner">
-        <div className="banner">
+        <div className="banner-store">
           <h2>Find liqour stores around you. </h2>
           <p>
             Get your favorite beer, wine & spirits delivered to your door step.
           </p>
-          {/* <div className="fire">
-            <input
-              type="text"
-              placeholder=" Enter Your Street or Home Address"
-            />{" "}
-            <h3>
-              {" "}
-              <BsSearch />
-            </h3>
-          </div> */}
         </div>
         <div className="store-category">
           <h3>Shop by Category</h3>
