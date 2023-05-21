ae117a52f6b9f1f633f7878d595bc2f9eee6bef4
Roche Harold <rocheharold13@gmail.com>
2023-05-21 08:48:48 +0300

    update changes

--- src/drinks.js
+++ src/drinks.js
@@ -5,7 +5,11 @@ export const beer = [
     titleImage: "./assets/preview.png",
     titleLink: "/Beer",
   },
-  { title: "Tomatoes", titleImage: "./assets/Cherry.png", titleLink: "/Wine" },
+  {
+    title: "Tomatoes",
+    titleImage: "./assets/vegetables.png",
+    titleLink: "/Wine",
+  },
   { title: "Whiskey", titleImage: "./assets/beer3.jpg", titleLink: "/Whiskey" },
   { title: "Tequila", titleImage: "./assets/beer4.jpg", titleLink: "/Tequila" },
   { title: "Vodka", titleImage: "./assets/beer5.jpg", titleLink: "/Vodka" },
