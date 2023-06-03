512eb7239d543228bf62aa1de5c88b25e7b96e06
Roche Harold <rocheharold13@gmail.com>
2023-06-03 16:39:48 +0300

    update changes

--- src/components/retailers/Retailers.jsx
+++ src/components/retailers/Retailers.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./Retailers.css";
 import { retail } from "../../miss";
+import { how, web } from "../../nav";
 
 const Retailers = () => {
   return (
     <div className="retailers-section">
       <div className="banner-retailers">
-        <h2>Unlock the Digital Bar: Bring Your Liquor Store to the Web! </h2>
+        <h2>Unlock the Digital Bar: Bring Your Grocery Store to the Web! </h2>
         <p>
           Having an internet presence for your business is crucial, and we can
-          assist you with selling more alcohol online.
+          assist you with selling more organic farm produce online.
         </p>
       </div>
       <div className="retailers-grow">
@@ -32,6 +33,37 @@ const Retailers = () => {
           </div>
           <div className="grow-text">
             <img src="./assets/Win.jpeg" alt="Proelio" loading="lazy" />
+          </div>
+        </div>{" "}
+        <div className="ret-how">
+          <h3>How We Help Your Business?</h3>
+          <div className="retailer-lis">
+            {how.map((item, index) => {
+              return (
+                <div key={index} className="retailer-list-one">
+                  <h2>{item.titleIcon}</h2>
+                  <h3>{item.title}</h3>
+                  <p>{item.titlelisting}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="store-website">
+          <div className="website-text">
+            <h3>Store Micro Website</h3>
+            {web.map((item, index) => {
+              return (
+                <div key={index} className="retailer-web">
+                  <h2>{item.titleIcon}</h2>
+                  <h3>{item.title}</h3>
+                  <p>{item.titlelisting}</p>
+                </div>
+              );
+            })}
+          </div>
+          <div className="website-photo">
+            <img src="././assets/Untitled1.png" alt="store" />
           </div>
         </div>
       </div>
--- src/nav.js
+++ src/nav.js
@@ -1,3 +1,12 @@
+import { ImFileEmpty } from "react-icons/im";
+import { MdFitScreen } from "react-icons/md";
+import { MdAdminPanelSettings } from "react-icons/md";
+import { MdOutlinePhonelink } from "react-icons/md";
+import { SlScreenDesktop } from "react-icons/sl";
+import { BsTools } from "react-icons/bs";
+import { CiMobile1 } from "react-icons/ci";
+import { RiShoppingCartFill } from "react-icons/ri";
+import { CgPlayListCheck } from "react-icons/cg";
 export const bar = [
   { title: "Home", navLink: "/" },
   { title: "Fruits", navLink: "/Beer" },
@@ -9,4 +18,62 @@ export const bar = [
   { title: "Retailers", navLink: "/Retailers" },
   { title: "Stores", navLink: "/Stores" },
   { title: "Promotions", navLink: "/Promotions" },
+];
+export const how = [
+  {
+    titleIcon: <ImFileEmpty />,
+    title: "Grocery Store Listing",
+    titlelisting:
+      "Get your grocery store listed on our platform and reach out to more customers and increase your sales.",
+  },
+  {
+    titleIcon: <MdFitScreen />,
+    title: "Branded Micro Website",
+    titlelisting:
+      "Complete control over the website content, products, services you want to showcase to your customers.",
+  },
+  {
+    titleIcon: <MdAdminPanelSettings />,
+    title: "Administrative Panel",
+    titlelisting:
+      "Easily manage orders, multiple store information, users, reports and much more from the admin panel.",
+  },
+  {
+    titleIcon: <MdOutlinePhonelink />,
+    title: "Store App",
+    titlelisting:
+      "App exclusively for liquor store owners to manage orders, products and its information..",
+  },
+  {
+    titleIcon: <SlScreenDesktop />,
+    title: "In Store TV Ads",
+    titlelisting:
+      "A unique way to showcase your products, event or brand on large TV sets installed in the store.",
+  },
+  {
+    titleIcon: <BsTools />,
+    title: "Marketing Toolkit",
+    titlelisting:
+      "Get personalized branding materials such as Banner, Door Stickers, Stand Sign, Flyers and more.",
+  },
+];
+export const web = [
+  {
+    titleIcon: <CiMobile1 />,
+    title: "Responsive Design",
+    titlelisting:
+      "The micro website is mobile friendly which ensures you stay ahead & never miss out on your target audience.",
+  },
+  {
+    titleIcon: <CgPlayListCheck />,
+    title: "Store Details",
+    titlelisting:
+      "Add and manage store details like store photos, timing, services and map location with ease.",
+  },
+  {
+    titleIcon: <RiShoppingCartFill />,
+    title: "Shopping Cart",
+    titlelisting:
+      "Get shopping cart integrated with your micro website that facilitates your visitors to place their orders.",
+  },
 ];
